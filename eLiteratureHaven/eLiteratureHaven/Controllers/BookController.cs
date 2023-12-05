77d8804a5fd358c34d5b65c7651726ed75d21ad7
Fadhl Aqbil Ramadhan <fadhl.hrs82@gmail.com>
2023-12-05 15:41:26 +0800

    Added framework for homepage and book details page

---
+++ eLiteratureHaven/eLiteratureHaven/Controllers/BookController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eLiteratureHaven.Models;
+
+namespace eLiteratureHaven.Controllers
+{
+    public class BookController : Controller
+    {
+        public ActionResult Book_details()
+        {
+            return View();
+        }
+    }
+}
