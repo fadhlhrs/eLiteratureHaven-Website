ec197e7f24dbf77aff3f0e3f6fad6958998ad947
Fadhl Aqbil Ramadhan <fadhl.hrs82@gmail.com>
2023-12-07 23:36:46 +0800

    tambah framework buat admin page & homepage + tambah admincontroller

---
+++ eLiteratureHaven/eLiteratureHaven/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eLiteratureHaven.Models;
+
+
+namespace eLiteratureHaven.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult Admin_home()
+        {
+            return View();
+        }
+    }
+}
